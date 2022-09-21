fa3a4a2918b2acd5a98298f55108dd5430d68f5e
oluwadaraprime <okunade0156@gmail.com>
2022-09-21 12:57:13 +0100

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c4b268e5f70ae5d53e9c69104644e2960780ae89',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4b268e5f70ae5d53e9c69104644e2960780ae89',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.4',
+      'version' => '6.6.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+    ),
+  ),
+);
